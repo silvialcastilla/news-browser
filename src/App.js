9018b0a98b897db3e6bd9e40c2f8756bbbd5b850
Tropical23 <silope0302@gmail.com>
2020-04-03 13:35:00 +0200

    proyecto maquetado

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
-import React, {useState, useEffect} from 'react';
-import Header from './components/Header'
-import Formulario from './components/Formulario'
-import ListadoNoticias from './components/ListadoNoticias';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import ListadoNoticias from "./components/ListadoNoticias";
 
 function App() {
-
   //definir la categoria y noticias
-  const [categoria, guardarCategoria] = useState('');
-  const [noticias, guardarNoticias] = useState([])
+  const [categoria, guardarCategoria] = useState("");
+  const [noticias, guardarNoticias] = useState([]);
 
-  useEffect(()=> {
-    const consultarAPI = async() => {
-      const url =`http://newsapi.org/v2/top-headlines?country=gb&category=${categoria}&apiKey=0f1d8bef18f84e0a9527f29f76d536db`;
-    
-      const respuesta = await fetch (url);
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `http://newsapi.org/v2/top-headlines?country=gb&category=${categoria}&apiKey=0f1d8bef18f84e0a9527f29f76d536db`;
+
+      const respuesta = await fetch(url);
       const noticias = await respuesta.json();
 
-      guardarNoticias(noticias.articles)
-    }
+      guardarNoticias(noticias.articles);
+    };
     consultarAPI();
-  }, [categoria])
- 
+  }, [categoria]);
+
   return (
     <>
-    <Header
-    titulo="Buscador de Noticias" />
-    <div className="container white">
-      <Formulario
-      guardarCategoria={guardarCategoria}
-      />
-      <ListadoNoticias
-        noticias={noticias} />
-    </div>
+      <Header titulo="Buscador de Noticias" />
+      <div className="container white">
+        <Formulario guardarCategoria={guardarCategoria} />
+        <ListadoNoticias noticias={noticias} />
+      </div>
     </>
   );
 }
